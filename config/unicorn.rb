d26e2ecd26c9c181901fbf5b462e26618244faa1
ttanaka9211 <ttanaka9211@hatchdogs.net>
2021-06-27 17:02:56 +0900

    first commit

---
+++ config/unicorn.rb
@@ -0,0 +1,33 @@
+#$worker  = 2
+$worker  = Integer(ENV["WEB_CONCURRENCY"] || 1)
+  $timeout = 30
+  $app_dir = "/shirasagi" #自分のアプリケーションまでのpath
+  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+  $stderr_log = File.expand_path 'log/unicorn.stderr.log', $app_dir
+  $stdout_log = File.expand_path 'log/unicorn.stdout.log', $app_dir
+  # set config
+  worker_processes  $worker
+  working_directory $app_dir
+  stderr_path $stderr_log
+  stdout_path $stdout_log
+  timeout $timeout
+  listen  $listen
+  pid $pid
+  # loading booster
+  preload_app true
+  # before starting processes
+  before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        Process.kill "QUIT", File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
+    end
+  end
+  # after finishing processes
+  after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  end
