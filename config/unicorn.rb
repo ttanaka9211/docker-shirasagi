b2f6eac3950a5a0fcdc9080affe73167b27d42d3
ttanaka9211 <ttanaka9211@hatchdogs.net>
2021-07-04 11:25:40 +0900

    修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,33 +1,25 @@
-#$worker  = 2
-$worker  = Integer(ENV["WEB_CONCURRENCY"] || 1)
-  $timeout = 30
-  $app_dir = "/var/www/shirasagi" #自分のアプリケーションまでのpath
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-  $stderr_log = File.expand_path 'log/unicorn.stderr.log', $app_dir
-  $stdout_log = File.expand_path 'log/unicorn.stdout.log', $app_dir
-  # set config
-  worker_processes  $worker
-  working_directory $app_dir
-  stderr_path $stderr_log
-  stdout_path $stdout_log
-  timeout $timeout
-  listen  $listen
-  pid $pid
-  # loading booster
-  preload_app true
-  # before starting processes
-  before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-    old_pid = "#{server.config[:pid]}.oldbin"
-    if old_pid != server.pid
-      begin
-        Process.kill "QUIT", File.read(old_pid).to_i
-      rescue Errno::ENOENT, Errno::ESRCH
-      end
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
+listen 3000
+timeout 120
+preload_app true
+
+root = File.expand_path("../../", __FILE__)
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.stderr.log"
+stdout_path "#{root}/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
-  # after finishing processes
-  after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
